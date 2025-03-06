b84a7088499f4d4def5776ddc8e9acb34ee9f474
rippunpirdasari27 <rippunsari27@sma.belajar.id>
2025-03-06 16:06:42 +0700

    Add files via upload

--- App.java
+++ App.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukan nama anda : ");
+        String name  = input.nextLine();
+        System.out.print("Masukan Nim anda : ");
+        String Nim = input.nextLine();
+        System.out.print("masukan umur anda : ");
+        int age = input.nextInt();
+        System.out.print("masukan tinggi badan anda : ");
+        double height = input.nextDouble();
+
+        System.out.print("\ndata yang anda masukan : ");
+        System.out.println("nama anda : " + name);
+        System.out.println("Nim anda : " + name);
+        System.out.println("umur anda: " +age );
+        System.out.println ("tinggi badan anda : " + height);
+
+        double hasilsoal1 = (age * 2 + 10/ 5);
+        System.out.println("hasil : " + hasilsoal1 );
+
+        boolean  umur = age > 18;
+        System.out.println("Masukan lebih dari 18:" + umur);
+
+        boolean tinggi = height > 160;
+        System.out.println("Masukan lebih dari 160:" + tinggi);
+
+        double usia = (double) + age;
+        int tinggibdn= (int) + height;
+
+        System.out.println("usia dalam double: " + usia);
+        System.out.println("tinggibdn dalam int: " + tinggibdn);
+
+
+
+
+
+
+
     }
 }
